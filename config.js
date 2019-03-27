328588d38b7fe4e98049128f98b644e8266cbc4a
ligiallucca <lilillucca@gmail.com>
2019-03-27 14:07:08 -0300

    alterações nos arquivos de login

---
+++ config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDO2aWGobByGVi87gqu5-CACqmXvWtY3dQ",
+  authDomain: "projeto-teste-aec44.firebaseapp.com",
+  databaseURL: "https://projeto-teste-aec44.firebaseio.com",
+  projectId: "projeto-teste-aec44",
+  storageBucket: "projeto-teste-aec44.appspot.com",
+  messagingSenderId: "653790958597"
+};
+firebase.initializeApp(config);
