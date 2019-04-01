95c97aa9bd830cb2e3e7e554b079244f1f628dd4
Vanessa <vanessayoshida92@gmail.com>
2019-04-01 14:16:41 -0300

    Botão excluir funcionando

--- src/post/post.js
+++ src/post/post.js
@@ -2,44 +2,50 @@ let database = firebase.database();
 let USER_ID = window.location.search.match(/\?id=(.*)/)[1];
 
 $(document).ready(function(){
+    getPostsBD();
+    $("#send-button").click(addPostsClick);
+});         
+
+function getPostsBD(){
     database.ref('/users/'+ USER_ID).once('value')
     .then(function(snapshot) {
         snapshot.forEach(function(childSnapshot) {
-
+            
             let childKey = childSnapshot.key;
             let childData = childSnapshot.val();
-            createPost(childData.text, childKey)
-           // $("#post-list").append(`<li>${childData.text}</li>`)
-          
+            
+            createListPost(childData.text, childKey);
         });
-        });
-    });
-    $("#send-button").click(function(event){
-        event.preventDefault();
+    });         
+}
 
-        let text = $("#post-input").val();
-        $("#post-input").val("");
-        
-        let newPostKey = database.ref('users/' + USER_ID).push({
-            text: text   
-        }); 
-
-        console.log(newPostKey.key);
-        
-        createPost(text);
-        //só para eu conseguir commitar =D
-    });
-
-    function createPost(text, key){
-        $("#post-list").append(`
-        <li>
-            <span>${text}</span>
-            <button class="delete-button" data-id="${key}">Excluir</button>
-        </li>
-        `);
+function addPostsClick(event){
+    event.preventDefault();
     
+    let newPost = $("#post-input").val();
+    $("#post-input").val("");
+    let postBD = addPostsBD(newPost);
+    
+    createListPost(newPost, postBD)
+}
 
-    $(`button[data-id=${key}]`).click(function(){
-        console.log(text);
-    })
-    }
+function addPostsBD(text){
+    return database.ref("users/" + USER_ID).push({
+        text: text
+    });
+}
+
+function createListPost(text, key){
+    $("#post-list").append(`
+    <li>
+    <span>${text}</span>
+    <button class="delete-button" data-id=${key}>Excluir</button>
+    </li>
+    `);
+    
+    $(`button[data-id="${key}"]`).click(function(){
+        database.ref("users/" + USER_ID + "/" + key).remove();
+        $(this).parent().remove();
+    });
+}
+
